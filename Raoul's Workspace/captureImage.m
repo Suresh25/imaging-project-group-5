fcc84d1ffcd4527c8c0c7600732c9ad26c9b996b
raoulhaa@gmail.com <raoulhaa@gmail.com@ccde3db6-ae14-6b68-5f29-66bbdb75d0e9>
2012-11-22 09:18:48 +0000

    Added liftDetect and Raoul's Workspace

---
+++ Raoul's Workspace/captureImage.m
@@ -0,0 +1,15 @@
+start(vid)
+data_color = getdata(vid, 1);
+stop(vid)
+a = joinchannels('rgb', dip_image(data_color))
+b = a{1}
+c = a{2}
+d = a{3}
+redThreshold = 130:70:200;
+greenThreshold = 20:30:50;
+blueThreshold = 20:30:50;
+r = threshold(b,'double',redThreshold)
+g = threshold(c,'double',greenThreshold)
+h = threshold(d,'double',blueThreshold)
+rghCombo = r&g&h
+rghCombo_labeled = label(rghCombo,Inf,200,0)
