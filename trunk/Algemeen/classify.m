3bd7843d560edbf365c8e92260f69c609efea516
raoulhaa@gmail.com <raoulhaa@gmail.com@ccde3db6-ae14-6b68-5f29-66bbdb75d0e9>
2012-12-03 09:06:46 +0000

    Added separate function files for each function in the function-scheme

---
+++ trunk/Algemeen/classify.m
@@ -0,0 +1,7 @@
+% classify(img)
+% img = Labeled DIPImage
+% Returns: Int array of length 2. First element is the # of persons in
+%          the lift. Second element is the # of persons out the lift
+function info = classify(img)
+    % PLACEHOLDER:
+    info = [2,2];
---
+++ trunk/Algemeen/enhance.m
@@ -0,0 +1,6 @@
+% enhance(img_data)
+% img_data = 3D RGB matrix containing pixel data
+% Returns: normalised & segmented image with lift- and person-segments
+function enhanced = enhance(img_data)
+    % PLACEHOLDER:
+    enhanced = joinchannels('rgb', img_data);
