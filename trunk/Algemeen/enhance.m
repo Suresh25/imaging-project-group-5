b7b84ffad9e4e23c41d091ccd539f686bfedd37c
raoulhaa@gmail.com <raoulhaa@gmail.com@ccde3db6-ae14-6b68-5f29-66bbdb75d0e9>
2012-12-03 09:16:09 +0000

    More function-files added and improved previous ones.

--- trunk/Algemeen/enhance.m
+++ trunk/Algemeen/enhance.m
@@ -1,6 +1,7 @@
 % enhance(img_data)
 % img_data = 3D RGB matrix containing pixel data
+% gui_handle = the handles of a GUI.
 % Returns: normalised & segmented image with lift- and person-segments
-function enhanced = enhance(img_data)
+function enhanced = enhance(img_data, gui_handle)
     % PLACEHOLDER:
     enhanced = joinchannels('rgb', img_data);
