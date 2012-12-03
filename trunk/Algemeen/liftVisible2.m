413b9a33a3b9165be789fc212cb63b4480b12c0d
raoulhaa@gmail.com <raoulhaa@gmail.com@ccde3db6-ae14-6b68-5f29-66bbdb75d0e9>
2012-12-03 10:55:03 +0000

    Small tweak to remove 'warning'

--- trunk/Algemeen/liftVisible2.m
+++ trunk/Algemeen/liftVisible2.m
@@ -11,14 +11,14 @@ function visible = liftVisible2(labeled_img)
     msr = measure(labeled_img, [], {'Size'}, [], 1, 0, 0);
     
     % Filter out small object-segments:
-    filtered_msr = zeros(1, size(msr));
+    filtered_msr = zeros(1, size(msr, 1));
     for i = 1:size(msr.size, 2)
         if msr(i).size >= segmentThreshold
             filtered_msr(1,i) = msr(i).size;
         end
     end
     
-    if sum(msr.size) < minArea
+    if sum(filtered_msr) < minArea
         visible = false;
         return
     end
