3eb20a4c807f0944440cff4edf9e8e4ccf85558e
raoulhaa@gmail.com <raoulhaa@gmail.com@ccde3db6-ae14-6b68-5f29-66bbdb75d0e9>
2012-12-03 10:51:38 +0000

    lift detection and lift visibility done

---
+++ trunk/Algemeen/liftVisible2.m
@@ -0,0 +1,26 @@
+function visible = liftVisible2(labeled_img)
+    % Some constants:
+    % minArea = The area of the lift when closed.
+    % segmentThreshold = Only objects with their size >= this will be
+    % processed.
+    
+    minArea = 28000;
+    segmentThreshold = 7000; 
+    
+    % Begin measurements:
+    msr = measure(labeled_img, [], {'Size'}, [], 1, 0, 0);
+    
+    % Filter out small object-segments:
+    filtered_msr = zeros(1, size(msr));
+    for i = 1:size(msr.size, 2)
+        if msr(i).size >= segmentThreshold
+            filtered_msr(1,i) = msr(i).size;
+        end
+    end
+    
+    if sum(msr.size) < minArea
+        visible = false;
+        return
+    end
+    
+    visible = true;
