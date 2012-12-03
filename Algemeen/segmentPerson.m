caa8ea5526fb826407f1cff6628df755cf839369
raoulhaa@gmail.com <raoulhaa@gmail.com@ccde3db6-ae14-6b68-5f29-66bbdb75d0e9>
2012-12-03 09:16:09 +0000

    More function-files added and improved previous ones.

---
+++ Algemeen/segmentPerson.m
@@ -0,0 +1,7 @@
+% segmentPerson(img)
+% img = Normalised DIPImage.
+% gui_handle = the handles of a GUI.
+% Returns: Segmented image (with person segments)
+
+function segmented = segmentPerson(img, gui_handle)
+    % TODO
